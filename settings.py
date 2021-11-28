bb861dfcf65c344b89620b8b749395e05782a56b
Marcos Vilela <87045821+marcosvile@users.noreply.github.com>
2021-11-28 15:42:12 -0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+ENV = {
+	"CONSUMER_KEY": "gPiZzk9nRg0YZABbk0lExWTlh",
+	"CONSUMER_SECRET": "6yKSWYoJFDnmI2PqzvZEfKwFTojp7763hdbcYilxr7XQ7ZaCTY",
+	"ACCESS_KEY": "1457323725305614337-C8EF4XYtxnu3QcZPsDUMicJrlTq7ve",
+	"ACCESS_SECRET": "yiAEDNzl1AXh0uKKVip9rCM2wVgnVtr7bcvBYbYyHukFR ",
+}
+
+#consumer keys: 
+	# api key gPiZzk9nRg0YZABbk0lExWTlh
+	# api secret key 6yKSWYoJFDnmI2PqzvZEfKwFTojp7763hdbcYilxr7XQ7ZaCTY
+
+#authentication tokens:
+		# bearer token AAAAAAAAAAAAAAAAAAAAAEZZWAEAAAAAMGoEHCMbZrVoA6PjY31qyP9tmXs%3Dq3gqI7mgXJPNbcj1xIeN1UNQH9g7mM6L1iv83gaHwIxNTjXDSd
+
+# access token 1457323725305614337-C8EF4XYtxnu3QcZPsDUMicJrlTq7ve
+# access token secret yiAEDNzl1AXh0uKKVip9rCM2wVgnVtr7bcvBYbYyHukFR
